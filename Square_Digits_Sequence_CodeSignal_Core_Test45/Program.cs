84d72123cf25fe2e5e03253c9fb0187c9bc892bb
Albert Hakobyan <abhakobyan24@gmail.com>
2021-09-18 12:18:29 +0400

    Добавьте файлы проекта.

---
+++ Square_Digits_Sequence_CodeSignal_Core_Test45/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Square_Digits_Sequence_CodeSignal_Core_Test45
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 16;
+            int result = squareDigitsSequence(n);
+            Console.WriteLine(result);
+        }
+
+
+        static int squareDigitsSequence(int a0)
+        {
+            int cur = a0;
+            HashSet<int> was = new HashSet<int>();
+
+            while (!was.Contains(cur))
+            {
+                was.Add(cur);
+                int next = 0;
+                while (cur > 0)
+                {
+                    next += (cur % 10) * (cur % 10);
+                    cur /= 10;
+                }
+                cur = next;
+            }
+
+            return was.Count + 1;
+        }
+
+    }
+}
